8f41b18b407e459db10543fdd298ae8275df180e
mauricioACV <mauricio.campusano@gmail.com>
2021-07-03 16:25:53 -0400

    Se agregan funcionalidades a botones en dos rutas, una para registrar un objeto con datos y otro para purgar esos datos. En resumen, podemos ver como las rutas comparten datos con useContext y como son notificados de los cambios en el context a los componentes hijos

--- src/components/09-useContext/HomeScreen.js
+++ src/components/09-useContext/HomeScreen.js
@@ -3,12 +3,15 @@ import { UserContext } from './UserContext'
 
 export const HomeScreen = () => {
 
-    const userContext = useContext(UserContext);
-    console.log(userContext)
+    const {user} = useContext(UserContext);
+    console.log(user)
     return (
         <div>
             <h1>HomeScreen</h1>
             <hr/>
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
         </div>
     )
 }
