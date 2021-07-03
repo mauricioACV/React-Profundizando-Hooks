50d7fc281fd5a3776abb97fc034f07efe6b53f68
mauricioACV <mauricio.campusano@gmail.com>
2021-07-03 15:48:57 -0400

    Implemetacion componente NavBar, ademas se utiliza navbar  de Bootstrap 4.5, repaso diferencias entre Link y NavLink de react-router-dom

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -3,7 +3,9 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-  } from "react-router-dom";
+    Redirect,
+} from "react-router-dom";
+import { NavBar } from './NavBar';
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
@@ -12,10 +14,13 @@ export const AppRouter = () => {
     return (
         <Router>
             <div>
+                <NavBar/>
                 <Switch>
                     <Route exact path="/" component={HomeScreen} />
                     <Route exact path="/about" component={AboutScreen} />
                     <Route exact path="/login" component={LoginScreen} />
+                    {/* <Route component={HomeScreen}/> */}
+                    <Redirect to="/"/>
                 </Switch>
             </div>
         </Router>
